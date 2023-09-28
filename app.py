b01ea69c13fa10032380a4ac5c1aa8479d419a1f
William <nearata@protonmail.com>
2023-09-28 11:46:46 +0200

    moved ipinfo utils out of main file

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from starlette.responses import JSONResponse
 from typing_extensions import Annotated
 from uvicorn import run
 
+from higeki.ipinfo import get_summary
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -48,29 +50,6 @@ class Item(BaseModel):
 
 def same_subnet():
     return IPv4Address("1.1.1.1") in IPv4Network("1.1.1.0/24")
-
-
-def get_summary(soup: BeautifulSoup, column: str) -> str:
-    if not (e := soup.find("span", string=column)):
-        return ""
-
-    if not (e1 := e.parent.find_next_sibling()):
-        return ""
-
-    return e1.get_text().strip().lower()
-
-
-def get_privacy_detection(soup: BeautifulSoup, column: str) -> Optional[bool]:
-    if not (e := soup.find("div", string=column)):
-        return None
-
-    if not (e1 := e.find_parent("div")):
-        return None
-
-    if not (e2 := e1.find("img")):
-        return None
-
-    return "right" in e2.get("src")
 
 
 async def check_ipinfo(item_id: IPvAnyAddress, request: Request) -> Optional[Item]:
---
+++ higeki/ipinfo.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from typing import Optional
+
+
+def get_privacy_detection(soup: BeautifulSoup, column: str) -> Optional[bool]:
+    if not (e := soup.find("div", string=column)):
+        return None
+
+    if not (e1 := e.find_parent("div")):
+        return None
+
+    if not (e2 := e1.find("img")):
+        return None
+
+    return "right" in e2.get("src")
+
+
+def get_summary(soup: BeautifulSoup, column: str) -> str:
+    if not (e := soup.find("span", string=column)):
+        return ""
+
+    if not (e1 := e.parent.find_next_sibling()):
+        return ""
+
+    return e1.get_text().strip().lower()
